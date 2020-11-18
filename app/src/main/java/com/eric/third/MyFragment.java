fc3c3558f39ed54b0d9f4d58bb827c107b8661d9
lgheric <saverneo@163.com>
2020-11-18 15:59:47 +0800

    The base usage of Fragment ,demo2

---
+++ app/src/main/java/com/eric/third/MyFragment.java
@@ -0,0 +1,26 @@
+package com.eric.third;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class MyFragment extends Fragment {
+
+    private String content;
+
+    public MyFragment(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_content,container,false);
+        TextView txt_content = view.findViewById(R.id.txt_content);
+        txt_content.setText(content);
+        return view;
+    }
+}
