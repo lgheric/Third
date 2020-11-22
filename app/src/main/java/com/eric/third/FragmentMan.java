61c5953c6e4e83f3f2503ca017297e418d1e581b
lgheric <saverneo@163.com>
2020-11-22 21:30:55 +0800

    Example of dynamically loading fragments from the web

---
+++ app/src/main/java/com/eric/third/FragmentMan.java
@@ -0,0 +1,24 @@
+package com.eric.third;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentMan extends Fragment {
+
+    public FragmentMan() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_man, container, false);
+        return view;
+    }
+
+}
--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -1,14 +1,67 @@
 package com.eric.third;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    //创建fragment对象
+    private FragmentMan fragmentMan;
+    private FragmentWoman fragmentWoman;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //绑定ID
+        bangID();
+
     }
 
+    private void bangID() {
+        //创建button对象
+        Button manBtn = findViewById(R.id.man_tv);
+        Button womanBtn = findViewById(R.id.woman_tv);
+        manBtn.setOnClickListener(this);
+        womanBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.man_tv:
+                //判断fragmentMan是否为空，无则创建fragment对象
+                if(fragmentMan==null){
+                    fragmentMan = new FragmentMan();
+                }
+                //创建FragmentManager对象
+                FragmentManager manager = getSupportFragmentManager();
+                //创建FragmentTransaction事务对象
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                //使用replace（将要替换位置的i的，替换的页面）方法实现页面的替换
+                fragmentTransaction.replace(R.id.shop_content,fragmentMan);
+                //提交事务
+                fragmentTransaction.commit();
+                break;
+            case R.id.woman_tv:
+                if(fragmentWoman==null){
+                    fragmentWoman = new FragmentWoman();
+                }
+                FragmentManager  manager1 = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction1 =manager1.beginTransaction();
+                fragmentTransaction1.replace(R.id.shop_content,fragmentWoman);
+                fragmentTransaction1.commit();
+                break;
+            default:
+                break;
+
+        }
+
+    }
 }
