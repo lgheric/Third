58acf3e6ad0a24df1807ffa49f7b6dfc0d4f46e9
lgheric <saverneo@163.com>
2020-11-22 22:39:44 +0800

    Realization of file reading and writing

--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -2,25 +2,74 @@ package com.eric.third;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private EditText editname;
+    private EditText editdetail;
+    private Button btnsave;
+    private Button btnclean;
+    private Button btnread;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
-        
-        Display dis = getWindowManager().getDefaultDisplay();
-        if(dis.getWidth() > dis.getHeight())
-        {
-            FragmentMan f1 = new FragmentMan();
-            getSupportFragmentManager().beginTransaction().replace(R.id.LinearLayout1, f1).commit();
-        } else {
-            FragmentWoman f2 = new FragmentWoman();
-            getSupportFragmentManager().beginTransaction().replace(R.id.LinearLayout1, f2).commit();
+        mContext = getApplicationContext();
+        bindViews();
+    }
+
+    private void bindViews() {
+        editdetail = findViewById(R.id.editdetail);
+        editname = findViewById(R.id.editname);
+        btnclean = findViewById(R.id.btnclean);
+        btnsave = findViewById(R.id.btnsave);
+        btnread = findViewById(R.id.btnread);
+
+        btnclean.setOnClickListener(this);
+        btnsave.setOnClickListener(this);
+        btnread.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnclean:
+                editdetail.setText("");
+                editname.setText("");
+                break;
+            case R.id.btnsave:
+                FileHelper fHelper = new FileHelper(mContext);
+                String filename = editname.getText().toString();
+                String filedetail = editdetail.getText().toString();
+                try {
+                    fHelper.save(filename, filedetail);
+                    Toast.makeText(mContext, "数据写入成功", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(mContext, "数据写入失败", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.btnread:
+                String detail = "";
+                FileHelper fHelper2 = new FileHelper(mContext);
+                try {
+                    String fname = editname.getText().toString();
+                    detail = fHelper2.read(fname);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(mContext, detail, Toast.LENGTH_SHORT).show();
+                break;
         }
     }
 }
