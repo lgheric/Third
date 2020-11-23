36691a0d27d8ca8f313ae8c928df0c805a3d5753
lgheric <saverneo@163.com>
2020-11-23 22:03:45 +0800

    Using SQLiteOpenHelper class to create database and versiion management

--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -1,80 +1,88 @@
 package com.eric.third;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.Map;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class    MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText editname;
-    private EditText editpasswd;
-    private Button btnlogin;
-    private String strname;
-    private String strpasswd;
-    private SharedHelper sh;
     private Context mContext;
-    private Context othercontext;
-    private SharedPreferences sp;
+    private Button btn_insert;
+    private Button btn_query;
+    private Button btn_update;
+    private Button btn_delete;
+    private SQLiteDatabase db;
+    private MyDBOpenHelper myDBHelper;
+    private StringBuilder sb;
+    private int i = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mContext = getApplicationContext();
-        sh = new SharedHelper(mContext);
+        mContext = MainActivity.this;
+        myDBHelper = new MyDBOpenHelper(mContext, "my.db", null, 1);
         bindViews();
     }
 
     private void bindViews() {
-        editname = (EditText)findViewById(R.id.editname);
-        editpasswd = (EditText)findViewById(R.id.editpasswd);
-        btnlogin = (Button)findViewById(R.id.btnlogin);
-        btnlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                strname = editname.getText().toString();
-                strpasswd = editpasswd.getText().toString();
-                strpasswd = Md5Util.getMD5(strpasswd);
-                sh.save(strname,strpasswd);
-            }
-        });
+        btn_insert = (Button) findViewById(R.id.btn_insert);
+        btn_query = (Button) findViewById(R.id.btn_query);
+        btn_update = (Button) findViewById(R.id.btn_update);
+        btn_delete = (Button) findViewById(R.id.btn_delete);
 
-        Button btnshow = (Button) findViewById(R.id.btnshow);
-        btnshow.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("WorldReadableFiles")
-            @Override
-            public void onClick(View v) {
-                //获得第一个应用的包名,从而获得对应的Context,需要对异常进行捕获
-                try {
-                    othercontext = createPackageContext("com.example.second", Context.CONTEXT_IGNORE_SECURITY);
-                } catch (PackageManager.NameNotFoundException e) {
-                    e.printStackTrace();
-                }
-                //根据Context取得对应的SharedPreferences
-                sp = othercontext.getSharedPreferences("second_sp", Context.MODE_WORLD_READABLE);
-                String name = sp.getString("username", "");
-                String passwd = sp.getString("passwd", "");
-                Toast.makeText(mContext, "Second的SharedPreference存的\n用户名为：" + name + "\n密码为：" + passwd, Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        btn_query.setOnClickListener(this);
+        btn_insert.setOnClickListener(this);
+        btn_update.setOnClickListener(this);
+        btn_delete.setOnClickListener(this);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Map<String,String> data = sh.read();
-        editname.setText(data.get("username"));
-        editpasswd.setText(data.get("passwd"));
+    public void onClick(View v) {
+        db = myDBHelper.getWritableDatabase();
+        switch (v.getId()) {
+            case R.id.btn_insert:
+                ContentValues values1 = new ContentValues();
+                values1.put("name", "呵呵~" + i);
+                i++;
+                //参数依次是：表名，强行插入null值得数据列的列名，一行记录的数据
+                db.insert("person", null, values1);
+                Toast.makeText(mContext, "插入完毕~", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_query:
+                sb = new StringBuilder();
+                //参数依次是:表名，列名，where约束条件，where中占位符提供具体的值，指定group by的列，进一步约束
+                //指定查询结果的排序方式
+                Cursor cursor = db.query("person", null, null, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        int pid = cursor.getInt(cursor.getColumnIndex("personid"));
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        sb.append("id：").append(pid).append("：").append(name).append("\n");
+                    } while (cursor.moveToNext());
+                }
+                cursor.close();
+                Toast.makeText(mContext, sb.toString(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_update:
+                ContentValues values2 = new ContentValues();
+                values2.put("name", "嘻嘻~");
+                //参数依次是表名，修改后的值，where条件，以及约束，如果不指定三四两个参数，会更改所有行
+                db.update("person", values2, "name = ?", new String[]{"呵呵~2"});
+                Toast.makeText(mContext, "更新完毕~", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_delete:
+                //参数依次是表名，以及where条件与约束
+                db.delete("person", "personid = ?", new String[]{"3"});
+                Toast.makeText(mContext, "删除完毕~", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
-
 }
