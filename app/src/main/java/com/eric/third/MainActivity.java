539f09854863be103f76d96b3c88b80ea8c78a65
lgheric <saverneo@163.com>
2020-12-03 22:35:07 +0800

    webservice by soap

--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -1,154 +1,161 @@
 package com.eric.third;
 
-import java.io.File;
-import com.eric.third.service.FileDownloadered;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText editpath;
-    private Button btndown;
-    private Button btnstop;
-    private TextView textresult;
-    private ProgressBar progressbar;
-    private static final int PROCESSING = 1;   //正在下载实时数据传输Message标志
-    private static final int FAILURE = -1;     //下载失败时的Message标志
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edit_param;
+    private Button btn_attribution;
+    private Button btn_weather;
+    private TextView txt_result;
+
+    private String city;
+    private String number;
+    private String result;
 
 
-    private final Handler handler = new Handler(Looper.myLooper()){
+    //定义获取手机信息的SoapAction与命名空间,作为常量
+    private static final String AddressnameSpace = "http://WebXml.com.cn/";
+    //天气查询相关参数
+    private static final String Weatherurl = "http://webservice.webxml.com.cn/WebServices/WeatherWS.asmx";
+    private static final String Weathermethod = "getWeather";
+    private static final String WeathersoapAction = "http://WebXml.com.cn/getWeather";
+    //归属地查询相关参数
+    private static final String Addressurl = "http://apis.juhe.cn/mobile/get";
+    private static final String Addressmethod = "getMobileCodeInfo";
+    private static final String AddresssoapAction = "http://WebXml.com.cn/getMobileCodeInfo";
+
+
+    //定义一个Handler用来更新页面：
+    private final Handler handler = new Handler(Objects.requireNonNull(Looper.myLooper())) {
         @SuppressLint("SetTextI18n")
-        @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
-                //下载时
-                case PROCESSING:
-                    int size = msg.getData().getInt("size");     //从消息中获取已经下载的数据长度
-                    progressbar.setProgress(size);         //设置进度条的进度
-                    //计算已经下载的百分比,此处需要转换为浮点数计算
-                    float num = (float)progressbar.getProgress() / (float)progressbar.getMax();
-                    int result = (int)(num * 100);     //把获取的浮点数计算结果转换为整数
-                    textresult.setText(result+ "%");   //把下载的百分比显示到界面控件上
-                    if(progressbar.getProgress() == progressbar.getMax()){ //下载完成时提示
-                        Toast.makeText(getApplicationContext(), "文件下载成功", Toast.LENGTH_LONG).show();
-                    }
+                case 0x001:
+                    txt_result.setText("结果显示：\n" + result);
+                    Toast.makeText(MainActivity.this, "获取天气信息成功", Toast.LENGTH_SHORT).show();
                     break;
-
-                case FAILURE:    //下载失败时提示
-                    Toast.makeText(getApplicationContext(), "文件下载失败", Toast.LENGTH_LONG).show();
+                case 0x002:
+                    txt_result.setText("结果显示：\n" + result);
+                    Toast.makeText(MainActivity.this, "号码归属地查询成功", Toast.LENGTH_SHORT).show();
                     break;
             }
+
         }
-
     };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        editpath = (EditText) findViewById(R.id.editpath);
-        btndown = (Button) findViewById(R.id.btndown);
-        btnstop = (Button) findViewById(R.id.btnstop);
-        textresult = (TextView) findViewById(R.id.textresult);
-        progressbar = (ProgressBar) findViewById(R.id.progressBar);
-        ButtonClickListener listener = new ButtonClickListener();
-        btndown.setOnClickListener(listener);
-        btnstop.setOnClickListener(listener);
-
-
+        bindViews();
     }
 
+    private void bindViews() {
+        edit_param = findViewById(R.id.edit_param);
+        btn_attribution = findViewById(R.id.btn_attribution);
+        btn_weather = findViewById(R.id.btn_weather);
+        txt_result = findViewById(R.id.txt_result);
+        btn_attribution.setOnClickListener(this);
+        btn_weather.setOnClickListener(this);
+    }
 
-    private final class ButtonClickListener implements View.OnClickListener{
-        @SuppressLint("NonConstantResourceId")
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.btndown:
-                    String path = editpath.getText().toString();
-                    if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
-                        File saveDir = Environment.getExternalStorageDirectory();
-                        download(path, saveDir);
-                    }else{
-                        Toast.makeText(getApplicationContext(), "sd卡读取失败", Toast.LENGTH_LONG).show();
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_weather:
+                new Thread() {
+                    @Override
+                    public void run() {
+                        getWether();
                     }
-                    btndown.setEnabled(false);
-                    btnstop.setEnabled(true);
-                    break;
-
-                case R.id.btnstop:
-                    exit();
-                    btndown.setEnabled(true);
-                    btnstop.setEnabled(false);
-                    break;
-            }
-        }
-        /*
-        由于用户的输入事件(点击button, 触摸屏幕....)是由主线程负责处理的，如果主线程处于工作状态，
-        此时用户产生的输入事件如果没能在5秒内得到处理，系统就会报“应用无响应”错误。
-        所以在主线程里不能执行一件比较耗时的工作，否则会因主线程阻塞而无法处理用户的输入事件，
-        导致“应用无响应”错误的出现。耗时的工作应该在子线程里执行。
-         */
-        private DownloadTask task;
-        /**
-         * 退出下载
-         */
-        public void exit(){
-            if(task!=null) task.exit();
-        }
-        private void download(String path, File saveDir) {//运行在主线程
-            task = new DownloadTask(path, saveDir);
-            new Thread(task).start();
-        }
-
-
-        /*
-         * UI控件画面的重绘(更新)是由主线程负责处理的，如果在子线程中更新UI控件的值，更新后的值不会重绘到屏幕上
-         * 一定要在主线程里更新UI控件的值，这样才能在屏幕上显示出来，不能在子线程中更新UI控件的值
-         */
-        private final class DownloadTask implements Runnable{
-            private final String path;
-            private final File saveDir;
-            private FileDownloadered loader;
-            public DownloadTask(String path, File saveDir) {
-                this.path = path;
-                this.saveDir = saveDir;
-            }
-            /**
-             * 退出下载
-             */
-            public void exit(){
-                if(loader!=null) loader.exit();
-            }
-
-            public void run() {
-                try {
-                    loader = new FileDownloadered(getApplicationContext(), path, saveDir, 3);
-                    progressbar.setMax(loader.getFileSize());//设置进度条的最大刻度
-                    loader.download(size -> {
-                        Message msg = new Message();
-                        msg.what = 1;
-                        msg.getData().putInt("size", size);
-                        handler.sendMessage(msg);
-                    });
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    handler.sendMessage(handler.obtainMessage(-1));
-                }
-            }
+                }.start();
+                break;
+            case R.id.btn_attribution:
+                new Thread(new Runnable() {
+                    public void run() {
+                        getland();
+                    }
+                }).start();
+                break;
         }
     }
+
+
+    //定义一个获取某城市天气信息的方法：
+    public void getWether() {
+        result = "";
+        SoapObject soapObject = new SoapObject(AddressnameSpace, Weathermethod);
+        soapObject.addProperty("theCityCode:", edit_param.getText().toString());
+        soapObject.addProperty("theUserID", "dbdf1580476240458784992289892b87");
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+        envelope.bodyOut = soapObject;
+        envelope.dotNet = true;
+        envelope.setOutputSoapObject(soapObject);
+        HttpTransportSE httpTransportSE = new HttpTransportSE(Weatherurl);
+        System.out.println("天气服务设置完毕,准备开启服务");
+        try {
+            httpTransportSE.call(WeathersoapAction, envelope);
+//            System.out.println("调用WebService服务成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+//            System.out.println("调用WebService服务失败");
+        }
+
+        //获得服务返回的数据,并且开始解析
+        SoapObject object = (SoapObject) envelope.bodyIn;
+        System.out.println("获得服务数据");
+        result = object.getProperty(1).toString();
+        handler.sendEmptyMessage(0x001);
+        System.out.println("发送完毕,textview显示天气信息");
+    }
+
+
+    //定义一个获取号码归属地的方法：
+    public void getland() {
+        result = "";
+        SoapObject soapObject = new SoapObject(AddressnameSpace, Addressmethod);
+        soapObject.addProperty("mobileCode", edit_param.getText().toString());
+        soapObject.addProperty("userid", "dbdf1580476240458784992289892b87");
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+        envelope.bodyOut = soapObject;
+        envelope.dotNet = true;
+        envelope.setOutputSoapObject(soapObject);
+        HttpTransportSE httpTransportSE = new HttpTransportSE(Addressurl);
+        //    System.out.println("号码信息设置完毕,准备开启服务");
+        try {
+            httpTransportSE.call(AddresssoapAction, envelope);
+            //System.out.println("调用WebService服务成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            //System.out.println("调用WebService服务失败");
+        }
+
+        //获得服务返回的数据,并且开始解析
+        SoapObject object = (SoapObject) envelope.bodyIn;//System.out.println("获得服务数据");
+        result = object.getProperty(0).toString();//System.out.println("获取信息完毕,向主线程发信息");
+        handler.sendEmptyMessage(0x001);
+        //System.out.println("发送完毕,textview显示天气信息");
+    }
+
+
 }
