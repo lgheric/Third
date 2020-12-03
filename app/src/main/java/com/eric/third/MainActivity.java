d63a23a19ad6922254d19c58879742605ab2ca88
lgheric <saverneo@163.com>
2020-12-03 22:56:37 +0800

    Load web page based on URL

--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -2,160 +2,59 @@ package com.eric.third;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.ksoap2.SoapEnvelope;
-import org.ksoap2.serialization.SoapObject;
-import org.ksoap2.serialization.SoapSerializationEnvelope;
-import org.ksoap2.transport.HttpTransportSE;
-
-import java.util.Objects;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText edit_param;
-    private Button btn_attribution;
-    private Button btn_weather;
-    private TextView txt_result;
+    private WebView webView;
+    private long exitTime = 0;
 
-    private String city;
-    private String number;
-    private String result;
-
-
-    //定义获取手机信息的SoapAction与命名空间,作为常量
-    private static final String AddressnameSpace = "http://WebXml.com.cn/";
-    //天气查询相关参数
-    private static final String Weatherurl = "http://webservice.webxml.com.cn/WebServices/WeatherWS.asmx";
-    private static final String Weathermethod = "getWeather";
-    private static final String WeathersoapAction = "http://WebXml.com.cn/getWeather";
-    //归属地查询相关参数
-    private static final String Addressurl = "http://apis.juhe.cn/mobile/get";
-    private static final String Addressmethod = "getMobileCodeInfo";
-    private static final String AddresssoapAction = "http://WebXml.com.cn/getMobileCodeInfo";
-
-
-    //定义一个Handler用来更新页面：
-    private final Handler handler = new Handler(Objects.requireNonNull(Looper.myLooper())) {
-        @SuppressLint("SetTextI18n")
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case 0x001:
-                    txt_result.setText("结果显示：\n" + result);
-                    Toast.makeText(MainActivity.this, "获取天气信息成功", Toast.LENGTH_SHORT).show();
-                    break;
-                case 0x002:
-                    txt_result.setText("结果显示：\n" + result);
-                    Toast.makeText(MainActivity.this, "号码归属地查询成功", Toast.LENGTH_SHORT).show();
-                    break;
-            }
-
-        }
-    };
-
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bindViews();
+        webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient() {
+            //设置在webView点击打开的新网页在当前界面显示,而不跳转到新的浏览器中
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.getSettings().setJavaScriptEnabled(true);  //设置WebView属性,运行执行js脚本
+        webView.loadUrl("https://www.baidu.com/");          //调用loadUrl方法为WebView加入链接
+        setContentView(webView);                           //调用Activity提供的setContentView将webView显示出来
     }
 
-    private void bindViews() {
-        edit_param = findViewById(R.id.edit_param);
-        btn_attribution = findViewById(R.id.btn_attribution);
-        btn_weather = findViewById(R.id.btn_weather);
-        txt_result = findViewById(R.id.txt_result);
-        btn_attribution.setOnClickListener(this);
-        btn_weather.setOnClickListener(this);
-    }
 
-    @SuppressLint("NonConstantResourceId")
+    //我们需要重写回退按钮的时间,当用户点击回退按钮：
+    //1.webView.canGoBack()判断网页是否能后退,可以则goback()
+    //2.如果不可以连续点击两次退出App,否则弹出提示Toast
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_weather:
-                new Thread() {
-                    @Override
-                    public void run() {
-                        getWether();
-                    }
-                }.start();
-                break;
-            case R.id.btn_attribution:
-                new Thread(new Runnable() {
-                    public void run() {
-                        getland();
-                    }
-                }).start();
-                break;
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            if ((System.currentTimeMillis() - exitTime) > 2000) {
+                Toast.makeText(getApplicationContext(), "再按一次退出程序",
+                        Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                super.onBackPressed();
+            }
+
         }
     }
 
+    @Override
+    public void onClick(View view) {
 
-    //定义一个获取某城市天气信息的方法：
-    public void getWether() {
-        result = "";
-        SoapObject soapObject = new SoapObject(AddressnameSpace, Weathermethod);
-        soapObject.addProperty("theCityCode:", edit_param.getText().toString());
-        soapObject.addProperty("theUserID", "dbdf1580476240458784992289892b87");
-        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-        envelope.bodyOut = soapObject;
-        envelope.dotNet = true;
-        envelope.setOutputSoapObject(soapObject);
-        HttpTransportSE httpTransportSE = new HttpTransportSE(Weatherurl);
-        System.out.println("天气服务设置完毕,准备开启服务");
-        try {
-            httpTransportSE.call(WeathersoapAction, envelope);
-//            System.out.println("调用WebService服务成功");
-        } catch (Exception e) {
-            e.printStackTrace();
-//            System.out.println("调用WebService服务失败");
-        }
-
-        //获得服务返回的数据,并且开始解析
-        SoapObject object = (SoapObject) envelope.bodyIn;
-        System.out.println("获得服务数据");
-        result = object.getProperty(1).toString();
-        handler.sendEmptyMessage(0x001);
-        System.out.println("发送完毕,textview显示天气信息");
     }
-
-
-    //定义一个获取号码归属地的方法：
-    public void getland() {
-        result = "";
-        SoapObject soapObject = new SoapObject(AddressnameSpace, Addressmethod);
-        soapObject.addProperty("mobileCode", edit_param.getText().toString());
-        soapObject.addProperty("userid", "dbdf1580476240458784992289892b87");
-        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-        envelope.bodyOut = soapObject;
-        envelope.dotNet = true;
-        envelope.setOutputSoapObject(soapObject);
-        HttpTransportSE httpTransportSE = new HttpTransportSE(Addressurl);
-        //    System.out.println("号码信息设置完毕,准备开启服务");
-        try {
-            httpTransportSE.call(AddresssoapAction, envelope);
-            //System.out.println("调用WebService服务成功");
-        } catch (Exception e) {
-            e.printStackTrace();
-            //System.out.println("调用WebService服务失败");
-        }
-
-        //获得服务返回的数据,并且开始解析
-        SoapObject object = (SoapObject) envelope.bodyIn;//System.out.println("获得服务数据");
-        result = object.getProperty(0).toString();//System.out.println("获取信息完毕,向主线程发信息");
-        handler.sendEmptyMessage(0x001);
-        //System.out.println("发送完毕,textview显示天气信息");
-    }
-
-
 }
