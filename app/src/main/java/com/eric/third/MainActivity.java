f64c11e5896c71ddf8c1bb3cbb51a10a6413fec5
lgheric <saverneo@163.com>
2020-12-03 23:09:55 +0800

    Setting WebView in layout code

--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -3,58 +3,92 @@ package com.eric.third;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private WebView webView;
+    private Button btn_back;
+    private TextView txt_title;
+    private Button btn_top;
+    private Button btn_refresh;
+    private WebView wView;
     private long exitTime = 0;
 
-    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView = new WebView(this);
-        webView.setWebViewClient(new WebViewClient() {
-            //设置在webView点击打开的新网页在当前界面显示,而不跳转到新的浏览器中
+        bindViews();
+    }
+
+    private void bindViews() {
+        btn_back = findViewById(R.id.btn_back);
+        txt_title = findViewById(R.id.txt_title);
+        btn_top = findViewById(R.id.btn_top);
+        btn_refresh = findViewById(R.id.btn_refresh);
+        wView = findViewById(R.id.wView);
+
+        btn_back.setOnClickListener(this);
+        btn_refresh.setOnClickListener(this);
+        btn_top.setOnClickListener(this);
+
+        wView.loadUrl("https://www.baidu.com");
+        wView.setWebChromeClient(new WebChromeClient() {
+            //这里设置获取到的网站title
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                txt_title.setText(title);
+            }
+        });
+
+        wView.setWebViewClient(new WebViewClient() {
+            //在webview里打开新链接
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
             }
         });
-        webView.getSettings().setJavaScriptEnabled(true);  //设置WebView属性,运行执行js脚本
-        webView.loadUrl("https://www.baidu.com/");          //调用loadUrl方法为WebView加入链接
-        setContentView(webView);                           //调用Activity提供的setContentView将webView显示出来
     }
 
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_back:
+                finish();          //关闭当前Activity
+                break;
+            case R.id.btn_refresh:
+                wView.reload();    //刷新当前页面
+                break;
+            case R.id.btn_top:
+                wView.setScrollY(0);   //滚动到顶部
+                break;
+        }
+    }
 
-    //我们需要重写回退按钮的时间,当用户点击回退按钮：
-    //1.webView.canGoBack()判断网页是否能后退,可以则goback()
-    //2.如果不可以连续点击两次退出App,否则弹出提示Toast
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
+        if (wView.canGoBack()) {
+            wView.goBack();
         } else {
             if ((System.currentTimeMillis() - exitTime) > 2000) {
-                Toast.makeText(getApplicationContext(), "再按一次退出程序",
-                        Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
                 exitTime = System.currentTimeMillis();
             } else {
-                super.onBackPressed();
+                finish();
             }
 
         }
     }
 
-    @Override
-    public void onClick(View view) {
 
-    }
 }
