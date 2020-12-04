1413930ad4f736c3234ac3c5d1fdf18febfc6824
lgheric <saverneo@163.com>
2020-12-04 16:06:47 +0800

    HTML calls three different dialog boxes through JS(Interaction foundation of WebView and javascratch)

--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -1,36 +1,80 @@
 package com.eric.third;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog.Builder;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class MainActivity extends AppCompatActivity {
 
     private WebView wView;
 
-    //@SuppressLint({"SetJavaScriptEnabled", "JavascriptInterface"})
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         wView = (WebView) findViewById(R.id.wView);
-        wView.loadUrl("file:///android_asset/demo1.html");
+
+        //获得WebSetting对象,支持js脚本,可访问文件,支持缩放,以及编码方式
         WebSettings webSettings = wView.getSettings();
-        //①设置WebView允许调用js
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setBuiltInZoomControls(true);
         webSettings.setDefaultTextEncodingName("UTF-8");
-        //②将object对象暴露给Js,调用addjavascriptInterface
-        wView.addJavascriptInterface(new MyObject(MainActivity.this), "myObj");
+        //设置WebChromeClient,处理网页中的各种js事件
+        wView.setWebChromeClient(new MyWebChromeClient());
+        wView.loadUrl("file:///android_asset/demo2.html");
     }
 
-    @Override
-    public void onClick(View view) {
+    //这里需要自定义一个类实现WebChromeClient类,并重写三种不同对话框的处理方法
+    //分别重写onJsAlert,onJsConfirm,onJsPrompt方法
+    class MyWebChromeClient extends WebChromeClient {
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+            //创建一个Builder来显示网页中的对话框
+            new Builder(MainActivity.this).setTitle("Alert对话框").setMessage(message)
+                    .setPositiveButton("确定", (dialog, which) -> result.confirm()).setCancelable(false).show();
+            return true;
+        }
+
+        @Override
+        public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
+            new Builder(MainActivity.this).setTitle("Confirm对话框").setMessage(message)
+                    .setPositiveButton("确定", (dialog, which) -> result.confirm())
+                    .setNegativeButton("取消", (dialog, which) -> result.cancel()).setCancelable(false).show();
+            return true;
+        }
+
+        @Override
+        public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, final JsPromptResult result) {
+            //①获得一个LayoutInflater对象factory,加载指定布局成相应对象
+            final LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+            final View myview = inflater.inflate(R.layout.prompt_view, null);
+            //设置TextView对应网页中的提示信息,edit设置来自于网页的默认文字
+            ((TextView) myview.findViewById(R.id.text)).setText(message);
+            ((EditText) myview.findViewById(R.id.edit)).setText(defaultValue);
+            //定义对话框上的确定按钮
+            new Builder(MainActivity.this).setTitle("Prompt对话框").setView(myview)
+                    .setPositiveButton("确定", (dialog, which) -> {
+                        //单机确定后取得输入的值,传给网页处理
+                        String value = ((EditText) myview.findViewById(R.id.edit)).getText().toString();
+                        result.confirm(value);
+                    })
+                    .setNegativeButton("取消", (dialog, which) -> result.cancel()).show();
+            return true;
+        }
 
     }
 }
