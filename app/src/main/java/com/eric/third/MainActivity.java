7e4371dbedb99a4e8a07b84f5e0f37f2ce5ceb03
lgheric <saverneo@163.com>
2020-12-12 00:51:26 +0800

    The base usage of SoundPool

--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -1,100 +1,115 @@
 package com.eric.third;
 
 import android.annotation.SuppressLint;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
-import android.os.FileUtils;
-import android.util.Log;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.File;
+import java.util.HashMap;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-
-    private WebView wView;
-    private Button btn_clear_cache;
-    private Button btn_refresh;
-    private static final String APP_CACHE_DIRNAME = "/webcache"; // web缓存目录
-    private static final String URL = "http://blog.csdn.cn/coder_pig";
+    private Button btn_play1;
+    private Button btn_play2;
+    private Button btn_play3;
+    private Button btn_play4;
+    private Button btn_play5;
+    private Button btn_release;
+    private AssetManager aManager;
+    private SoundPool mSoundPool = null;
+    private HashMap<Integer, Integer> soundID = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        handleWebView();
+
+//        try {
+//            mSoundPool = new SoundPool(1, AudioManager.STREAM_SYSTEM, 1);
+//            int sid = mSoundPool.load(this,R.raw.duang,1);
+//            mSoundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+//                @Override
+//                public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+//                    Toast.makeText(MainActivity.this,"加特技准备完毕~",Toast.LENGTH_SHORT).show();
+//                    mSoundPool.play(sid,1,1,0,1,1);
+//                }
+//            });
+//
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+        aManager = getAssets();
+        try {
+            initSP();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        bindViews();
     }
 
+    private void bindViews() {
+        btn_play1 = findViewById(R.id.btn_play1);
+        btn_play2 = findViewById(R.id.btn_play2);
+        btn_play3 = findViewById(R.id.btn_play3);
+        btn_play4 = findViewById(R.id.btn_play4);
+        btn_play5 = findViewById(R.id.btn_play5);
+        btn_release = findViewById(R.id.btn_release);
 
-    @SuppressLint("SetJavaScriptEnabled")
-    private void handleWebView(){
+        btn_play1.setOnClickListener(this);
+        btn_play2.setOnClickListener(this);
+        btn_play3.setOnClickListener(this);
+        btn_play4.setOnClickListener(this);
+        btn_play5.setOnClickListener(this);
+        btn_release.setOnClickListener(this);
 
-        wView = findViewById(R.id.wView);
-        btn_clear_cache = findViewById(R.id.btn_clear_cache);
-        btn_refresh = findViewById(R.id.btn_refresh);
-        wView.loadUrl(URL);
-        wView.setWebViewClient(new WebViewClient() {
-            //设置在webView点击打开的新网页在当前界面显示,而不跳转到新的浏览器中
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-
-            @Override
-            public void onReceivedError(WebView view, int errorCode, String description,
-                                        String failingUrl) {
-                super.onReceivedError(view, errorCode, description, failingUrl);
-                wView.loadUrl("file:///android_asset/error.html");
-            }
-
-        });
-        WebSettings settings = wView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        //设置缓存模式
-        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-        // 开启DOM storage API 功能
-        settings.setDomStorageEnabled(true);
-        // 开启database storage API功能
-        settings.setDatabaseEnabled(true);
-        String cacheDirPath = getFilesDir().getAbsolutePath() + APP_CACHE_DIRNAME;
-        Log.i("cachePath", cacheDirPath);
-        //File f = new File(cacheDirPath);
-        //if(!f.exists())f.mkdirs();
-        // 设置数据库缓存路径
-        settings.setAppCachePath(cacheDirPath);
-        settings.setAppCacheEnabled(true);
-        //Log.i("databasepath", settings.getDatabasePath());
-
-        btn_clear_cache.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                wView.clearCache(true);
-            }
-        });
-
-        btn_refresh.setOnClickListener(v -> wView.reload());//lambda 表达式
-//        btn_refresh.setOnClickListener(new View.OnClickListener() {//普通表达式
-//            @Override
-//            public void onClick(View v) {
-//                wView.reload();
-//            }
-//        });
     }
 
-    //重写回退按钮的点击事件
+    private void initSP() throws Exception{
+        //设置最多可容纳5个音频流，音频的品质为5
+        mSoundPool = new SoundPool(5, AudioManager.STREAM_SYSTEM, 5);
+        soundID.put(1, mSoundPool.load(this, R.raw.duang, 1));
+        soundID.put(2 , mSoundPool.load(getAssets().openFd("biaobiao.mp3") , 1));  //需要捕获IO异常
+        soundID.put(3, mSoundPool.load(this, R.raw.duang, 1));
+        soundID.put(4, mSoundPool.load(this, R.raw.duang, 1));
+        soundID.put(5, mSoundPool.load(this, R.raw.duang, 1));
+        mSoundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                Toast.makeText(MainActivity.this,"加特技准备完毕~",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @SuppressLint("NonConstantResourceId")
     @Override
-    public void onBackPressed() {
-        if(wView.canGoBack()){
-            wView.goBack();
-        }else{
-            super.onBackPressed();
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_play1:
+                mSoundPool.play(soundID.get(1), 1, 1, 0, 0, 1);
+                break;
+            case R.id.btn_play2:
+                mSoundPool.play(soundID.get(2), 1, 1, 0, 0, 1);
+                break;
+            case R.id.btn_play3:
+                mSoundPool.play(soundID.get(3), 1, 1, 0, 0, 1);
+                break;
+            case R.id.btn_play4:
+                mSoundPool.play(soundID.get(4), 1, 1, 0, 0, 1);
+                break;
+            case R.id.btn_play5:
+                mSoundPool.play(soundID.get(5), 1, 1, 0, 0, 1);
+                break;
+            case R.id.btn_release:
+                mSoundPool.release();   //回收SoundPool资源
+                break;
         }
     }
+
 }
