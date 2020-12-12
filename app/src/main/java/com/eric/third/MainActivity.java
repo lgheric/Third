ce6124c213ce69ff2324f12dca7a72d82a317b18
lgheric <saverneo@163.com>
2020-12-13 02:25:40 +0800

    Playing video with MediaPlayer via SurfaceView

--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -1,67 +1,94 @@
 package com.eric.third;
 
 import android.annotation.SuppressLint;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SurfaceHolder.Callback {
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+        private MediaPlayer mPlayer = null;
+        private SurfaceView sfv_show;
+        private SurfaceHolder surfaceHolder;
+        private Button btn_start;
+        private Button btn_pause;
+        private Button btn_stop;
 
-    private Button btn_play;
-    private Button btn_pause;
-    private Button btn_stop;
-    private MediaPlayer mPlayer = null;
-    private boolean isRelease = true;   //判断是否MediaPlayer是否释放的标志
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            bindViews();
+        }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        bindViews();
-    }
+        private void bindViews() {
+            sfv_show = findViewById(R.id.sfv_show);
+            btn_start = findViewById(R.id.btn_start);
+            btn_pause = findViewById(R.id.btn_pause);
+            btn_stop = findViewById(R.id.btn_stop);
 
-    private void bindViews() {
-        btn_play = findViewById(R.id.btn_play);
-        btn_pause = findViewById(R.id.btn_pause);
-        btn_stop = findViewById(R.id.btn_stop);
+            btn_start.setOnClickListener(this);
+            btn_pause.setOnClickListener(this);
+            btn_stop.setOnClickListener(this);
 
-        btn_play.setOnClickListener(this);
-        btn_pause.setOnClickListener(this);
-        btn_stop.setOnClickListener(this);
-    }
+            //初始化SurfaceHolder类，SurfaceView的控制器
+            surfaceHolder = sfv_show.getHolder();
+            surfaceHolder.addCallback(this);
+            surfaceHolder.setFixedSize(320, 220);   //显示的分辨率,不设置为视频默认
 
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_play:
-                if(isRelease){
-                    mPlayer = MediaPlayer.create(this,R.raw.fly);
-                    isRelease = false;
-                }
-                mPlayer.start();   //开始播放
-                btn_play.setEnabled(false);
-                btn_pause.setEnabled(true);
-                btn_stop.setEnabled(true);
-                break;
-            case R.id.btn_pause:
-                mPlayer.pause();     //停止播放
-                btn_play.setEnabled(true);
-                btn_pause.setEnabled(false);
-                btn_stop.setEnabled(false);
-                break;
-            case R.id.btn_stop:
-                mPlayer.reset();     //重置MediaPlayer
-                mPlayer.release();   //释放MediaPlayer
-                isRelease = true;
-                btn_play.setEnabled(true);
-                btn_pause.setEnabled(false);
-                btn_stop.setEnabled(false);
-                break;
+        }
+
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_start:
+                    mPlayer.start();
+                    btn_start.setEnabled(false);
+                    btn_pause.setEnabled(true);
+                    btn_stop.setEnabled(true);
+                    break;
+                case R.id.btn_pause:
+                    mPlayer.pause();
+                    btn_start.setEnabled(true);
+                    btn_pause.setEnabled(false);
+                    btn_stop.setEnabled(false);
+                    break;
+                case R.id.btn_stop:
+                    mPlayer.stop();
+                    btn_start.setEnabled(true);
+                    btn_pause.setEnabled(false);
+                    btn_stop.setEnabled(false);
+                    break;
+            }
+        }
+
+        @Override
+        public void surfaceCreated(@NonNull SurfaceHolder holder) {
+            mPlayer = MediaPlayer.create(MainActivity.this, R.raw.lesson);
+            mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mPlayer.setDisplay(surfaceHolder);    //设置显示视频显示在SurfaceView上
+        }
+
+        @Override
+        public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {}
+
+        @Override
+        public void surfaceDestroyed(@NonNull SurfaceHolder holder) {}
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            if (mPlayer.isPlaying()) {
+                mPlayer.stop();
+            }
+            mPlayer.release();
         }
     }
-}
