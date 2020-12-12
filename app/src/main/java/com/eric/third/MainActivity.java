391b56f60f754fdfe6141ffe583ab71f8691c3c8
lgheric <saverneo@163.com>
2020-12-13 00:58:32 +0800

    Playing audio with MedioPlayer

--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -1,115 +1,67 @@
 package com.eric.third;
 
 import android.annotation.SuppressLint;
-import android.content.res.AssetManager;
-import android.media.AudioManager;
-import android.media.SoundPool;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.HashMap;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Button btn_play1;
-    private Button btn_play2;
-    private Button btn_play3;
-    private Button btn_play4;
-    private Button btn_play5;
-    private Button btn_release;
-    private AssetManager aManager;
-    private SoundPool mSoundPool = null;
-    private HashMap<Integer, Integer> soundID = new HashMap<>();
+    private Button btn_play;
+    private Button btn_pause;
+    private Button btn_stop;
+    private MediaPlayer mPlayer = null;
+    private boolean isRelease = true;   //判断是否MediaPlayer是否释放的标志
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        try {
-//            mSoundPool = new SoundPool(1, AudioManager.STREAM_SYSTEM, 1);
-//            int sid = mSoundPool.load(this,R.raw.duang,1);
-//            mSoundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
-//                @Override
-//                public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-//                    Toast.makeText(MainActivity.this,"加特技准备完毕~",Toast.LENGTH_SHORT).show();
-//                    mSoundPool.play(sid,1,1,0,1,1);
-//                }
-//            });
-//
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-        aManager = getAssets();
-        try {
-            initSP();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         bindViews();
     }
 
     private void bindViews() {
-        btn_play1 = findViewById(R.id.btn_play1);
-        btn_play2 = findViewById(R.id.btn_play2);
-        btn_play3 = findViewById(R.id.btn_play3);
-        btn_play4 = findViewById(R.id.btn_play4);
-        btn_play5 = findViewById(R.id.btn_play5);
-        btn_release = findViewById(R.id.btn_release);
+        btn_play = findViewById(R.id.btn_play);
+        btn_pause = findViewById(R.id.btn_pause);
+        btn_stop = findViewById(R.id.btn_stop);
 
-        btn_play1.setOnClickListener(this);
-        btn_play2.setOnClickListener(this);
-        btn_play3.setOnClickListener(this);
-        btn_play4.setOnClickListener(this);
-        btn_play5.setOnClickListener(this);
-        btn_release.setOnClickListener(this);
-
-    }
-
-    private void initSP() throws Exception{
-        //设置最多可容纳5个音频流，音频的品质为5
-        mSoundPool = new SoundPool(5, AudioManager.STREAM_SYSTEM, 5);
-        soundID.put(1, mSoundPool.load(this, R.raw.duang, 1));
-        soundID.put(2 , mSoundPool.load(getAssets().openFd("biaobiao.mp3") , 1));  //需要捕获IO异常
-        soundID.put(3, mSoundPool.load(this, R.raw.duang, 1));
-        soundID.put(4, mSoundPool.load(this, R.raw.duang, 1));
-        soundID.put(5, mSoundPool.load(this, R.raw.duang, 1));
-        mSoundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
-            @Override
-            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-                Toast.makeText(MainActivity.this,"加特技准备完毕~",Toast.LENGTH_SHORT).show();
-            }
-        });
+        btn_play.setOnClickListener(this);
+        btn_pause.setOnClickListener(this);
+        btn_stop.setOnClickListener(this);
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.btn_play1:
-                mSoundPool.play(soundID.get(1), 1, 1, 0, 0, 1);
+            case R.id.btn_play:
+                if(isRelease){
+                    mPlayer = MediaPlayer.create(this,R.raw.fly);
+                    isRelease = false;
+                }
+                mPlayer.start();   //开始播放
+                btn_play.setEnabled(false);
+                btn_pause.setEnabled(true);
+                btn_stop.setEnabled(true);
                 break;
-            case R.id.btn_play2:
-                mSoundPool.play(soundID.get(2), 1, 1, 0, 0, 1);
+            case R.id.btn_pause:
+                mPlayer.pause();     //停止播放
+                btn_play.setEnabled(true);
+                btn_pause.setEnabled(false);
+                btn_stop.setEnabled(false);
                 break;
-            case R.id.btn_play3:
-                mSoundPool.play(soundID.get(3), 1, 1, 0, 0, 1);
-                break;
-            case R.id.btn_play4:
-                mSoundPool.play(soundID.get(4), 1, 1, 0, 0, 1);
-                break;
-            case R.id.btn_play5:
-                mSoundPool.play(soundID.get(5), 1, 1, 0, 0, 1);
-                break;
-            case R.id.btn_release:
-                mSoundPool.release();   //回收SoundPool资源
+            case R.id.btn_stop:
+                mPlayer.reset();     //重置MediaPlayer
+                mPlayer.release();   //释放MediaPlayer
+                isRelease = true;
+                btn_play.setEnabled(true);
+                btn_pause.setEnabled(false);
+                btn_stop.setEnabled(false);
                 break;
         }
     }
-
 }
