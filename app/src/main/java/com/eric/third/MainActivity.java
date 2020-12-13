25ba12166eb31ecf1cc35e09107f7968dcd64a8b
lgheric <saverneo@163.com>
2020-12-13 23:37:52 +0800

    Take a picture and save it to sdcard.FileProvider.

--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -2,54 +2,89 @@ package com.eric.third;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
 
     private ImageView img_show;
-    private Button btn_cam;
-
+    private File currentImageFile = null;
+    private Uri uri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         bindViews();
     }
 
     private void bindViews() {
-        btn_cam = findViewById(R.id.btn_cam);
-        img_show = findViewById(R.id.img_show);
+        img_show =  findViewById(R.id.img_show);
+        Button btn_start = findViewById(R.id.btn_start);
 
-        btn_cam.setOnClickListener(this);
+        btn_start.setOnClickListener(v -> {
+            File dir = new File(Environment.getExternalStorageDirectory(),"pictures");
+            if(!dir.exists()){
+                Boolean rs = dir.mkdirs();
+                System.out.println(dir+" 创建结果："+rs);
+
+            }
+            currentImageFile = new File(dir,System.currentTimeMillis() + ".jpg");
+
+            System.out.println(currentImageFile);
+            if(!currentImageFile.exists()){
+                try {
+                    Boolean rs = currentImageFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                System.out.println("exists file:"+currentImageFile);
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                uri = FileProvider.getUriForFile(MainActivity.this, "com.eric.third.fileProvider", currentImageFile);
+            } else {
+                uri = Uri.fromFile(currentImageFile);
+            }
+            Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            it.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+            startActivityForResult(it, Activity.DEFAULT_KEYS_DIALER);
+        });
     }
 
-    //重写onActivityResult方法
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        System.out.println("requestCode:"+requestCode);
-        System.out.println("resultCode:"+resultCode);
-        if (resultCode == Activity.RESULT_OK) {
-            Bundle bundle = data.getExtras();
-            assert bundle != null;
-            Bitmap bitmap = (Bitmap) bundle.get("data");
-            img_show.setImageBitmap(bitmap);
+        if (requestCode == Activity.DEFAULT_KEYS_DIALER) {
+//            Bundle bundle = data.getExtras();
+//            Bitmap bitmap = (Bitmap) bundle.get("data");
+//            img_show.setImageBitmap(bitmap);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                uri = FileProvider.getUriForFile(MainActivity.this, "com.eric.third.fileProvider", currentImageFile);
+            } else {
+                uri = Uri.fromFile(currentImageFile);
+            }
+            System.out.println("[uri]"+uri);
+            img_show.setImageURI(uri);
         }
-    }
 
-    @Override
-    public void onClick(View v) {
-        //
-        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(it,Activity.DEFAULT_KEYS_DIALER);
 
+//        switch (requestCode){
+//            case REQUEST_CODE_TAKE_PICTURE:
+//                img_show.setImageURI(Uri.fromFile(currentImageFile));
+//                break;
+//        }
     }
 }
