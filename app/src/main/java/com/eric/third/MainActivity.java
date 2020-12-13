4fef12304437f5176b827d9593811fc0334474ff
lgheric <saverneo@163.com>
2020-12-14 00:22:09 +0800

    Recording with MediaRecorder

--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -1,90 +1,77 @@
 package com.eric.third;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView img_show;
-    private File currentImageFile = null;
-    private Uri uri;
+    private Button btn_control;
+    private boolean isStart = false;
+    private MediaRecorder mr = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bindViews();
-    }
-
-    private void bindViews() {
-
-        img_show =  findViewById(R.id.img_show);
-        Button btn_start = findViewById(R.id.btn_start);
-
-        btn_start.setOnClickListener(v -> {
-            File dir = new File(Environment.getExternalStorageDirectory(),"pictures");
-            if(!dir.exists()){
-                Boolean rs = dir.mkdirs();
-                System.out.println(dir+" 创建结果："+rs);
-
-            }
-            currentImageFile = new File(dir,System.currentTimeMillis() + ".jpg");
-
-            System.out.println(currentImageFile);
-            if(!currentImageFile.exists()){
-                try {
-                    Boolean rs = currentImageFile.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        btn_control = (Button) findViewById(R.id.btn_control);
+        btn_control.setOnClickListener(v -> {
+            if(!isStart){
+                startRecord();
+                btn_control.setText("停止录制");
+                isStart = true;
             }else{
-                System.out.println("exists file:"+currentImageFile);
+                stopRecord();
+                btn_control.setText("开始录制");
+                isStart = false;
             }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                uri = FileProvider.getUriForFile(MainActivity.this, "com.eric.third.fileProvider", currentImageFile);
-            } else {
-                uri = Uri.fromFile(currentImageFile);
-            }
-            Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            it.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-            startActivityForResult(it, Activity.DEFAULT_KEYS_DIALER);
         });
     }
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == Activity.DEFAULT_KEYS_DIALER) {
-//            Bundle bundle = data.getExtras();
-//            Bitmap bitmap = (Bitmap) bundle.get("data");
-//            img_show.setImageBitmap(bitmap);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                uri = FileProvider.getUriForFile(MainActivity.this, "com.eric.third.fileProvider", currentImageFile);
-            } else {
-                uri = Uri.fromFile(currentImageFile);
+    //开始录制
+    private void startRecord(){
+        if(mr == null){
+            File dir = new File(Environment.getExternalStorageDirectory(),"sounds");
+            if(!dir.exists()){
+                Boolean rs = dir.mkdirs();
             }
-            System.out.println("[uri]"+uri);
-            img_show.setImageURI(uri);
+            File soundFile = new File(dir,System.currentTimeMillis()+".amr");
+            if(!soundFile.exists()){
+                try {
+                    Boolean rs =  soundFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            mr = new MediaRecorder();
+            mr.setAudioSource(MediaRecorder.AudioSource.MIC);  //音频输入源
+            mr.setOutputFormat(MediaRecorder.OutputFormat.AMR_WB);   //设置输出格式
+            mr.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_WB);   //设置编码格式
+            mr.setOutputFile(soundFile.getAbsolutePath());
+            try {
+                mr.prepare();
+                mr.start();  //开始录制
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
+    }
 
-
-//        switch (requestCode){
-//            case REQUEST_CODE_TAKE_PICTURE:
-//                img_show.setImageURI(Uri.fromFile(currentImageFile));
-//                break;
-//        }
+    //停止录制，资源释放
+    private void stopRecord(){
+        if(mr != null){
+            mr.stop();
+            mr.release();
+            mr = null;
+        }
     }
 }
