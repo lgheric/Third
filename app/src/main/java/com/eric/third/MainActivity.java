bd457130add4ffd049b3396229a4a03ca3681fe0
lgheric <saverneo@163.com>
2020-12-13 17:34:35 +0800

    crate a custom camera page.

--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -1,55 +1,110 @@
 package com.eric.third;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.hardware.Camera;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-    private ImageView img_show;
-    private Button btn_cam;
+public class MainActivity extends AppCompatActivity {
+
+    private SurfaceView sfv_preview;
+    private Button btn_take;
+    private Camera camera = null;
+    private final SurfaceHolder.Callback cpHolderCallback = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceCreated(@NonNull SurfaceHolder holder) {
+            startPreview();
+        }
+
+        @Override
+        public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+            stopPreview();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         bindViews();
     }
 
     private void bindViews() {
-        btn_cam = findViewById(R.id.btn_cam);
-        img_show = findViewById(R.id.img_show);
+        sfv_preview = findViewById(R.id.sfv_preview);
+        btn_take = findViewById(R.id.btn_take);
+        sfv_preview.getHolder().addCallback(cpHolderCallback);
 
-        btn_cam.setOnClickListener(this);
+        btn_take.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                camera.takePicture(null, null, new Camera.PictureCallback() {
+                    @Override
+                    public void onPictureTaken(byte[] data, Camera camera) {
+                        String path = "";
+                        if ((path = saveFile(data)) != null) {
+                            Intent it = new Intent(MainActivity.this, PreviewActivity.class);
+                            it.putExtra("path", path);
+                            startActivity(it);
+                        } else {
+                            Toast.makeText(MainActivity.this, "保存照片失败", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 
-    //重写onActivityResult方法
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        System.out.println("requestCode:"+requestCode);
-        System.out.println("resultCode:"+resultCode);
-        if (resultCode == Activity.RESULT_OK) {
-            Bundle bundle = data.getExtras();
-            assert bundle != null;
-            Bitmap bitmap = (Bitmap) bundle.get("data");
-            img_show.setImageBitmap(bitmap);
+    //保存临时文件的方法
+    private String saveFile(byte[] bytes){
+        try {
+            File file = File.createTempFile("img","");
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(bytes);
+            fos.flush();
+            fos.close();
+            String s = file.getAbsolutePath();
+            System.out.println("path:"+s);
+            return s;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+
+    //开始预览
+    private void startPreview(){
+        camera = Camera.open();
+        try {
+            camera.setPreviewDisplay(sfv_preview.getHolder());
+            camera.setDisplayOrientation(90);   //让相机旋转90度
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    @Override
-    public void onClick(View v) {
-        //
-        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(it,Activity.DEFAULT_KEYS_DIALER);
-
+    //停止预览
+    private void stopPreview() {
+        camera.stopPreview();
+        camera.release();
+        camera = null;
     }
+
 }
