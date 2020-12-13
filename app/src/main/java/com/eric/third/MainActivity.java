b0fe88c5c94e733434b5306ff3c297c9d73ddf23
lgheric <saverneo@163.com>
2020-12-13 16:50:22 +0800

    Take photos by calling the system's own carema

--- app/src/main/java/com/eric/third/MainActivity.java
+++ app/src/main/java/com/eric/third/MainActivity.java
@@ -1,62 +1,55 @@
 package com.eric.third;
 
-import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private VideoView videoView;
-    private Button btn_start;
-    private Button btn_pause;
-    private Button btn_stop;
+    private ImageView img_show;
+    private Button btn_cam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         bindViews();
     }
 
     private void bindViews() {
-        videoView = findViewById(R.id.videoView);
-        btn_start = findViewById(R.id.btn_start);
-        btn_pause = findViewById(R.id.btn_pause);
-        btn_stop = findViewById(R.id.btn_stop);
+        btn_cam = findViewById(R.id.btn_cam);
+        img_show = findViewById(R.id.img_show);
 
-        btn_start.setOnClickListener(this);
-        btn_pause.setOnClickListener(this);
-        btn_stop.setOnClickListener(this);
-
-        //根据文件路径播放
-        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            videoView.setVideoPath(Environment.getExternalStorageDirectory() + "/lesson.mp4");
-        }
-System.out.println(Environment.getExternalStorageDirectory() + "/lesson.mp4");
-        //读取放在raw目录下的文件
-        //videoView.setVideoURI(Uri.parse("android.resource://com.eric.third/" + R.raw.lesson));
-        videoView.setMediaController(new MediaController(this));
+        btn_cam.setOnClickListener(this);
     }
 
-    @SuppressLint("NonConstantResourceId")
+    //重写onActivityResult方法
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println("requestCode:"+requestCode);
+        System.out.println("resultCode:"+resultCode);
+        if (resultCode == Activity.RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            assert bundle != null;
+            Bitmap bitmap = (Bitmap) bundle.get("data");
+            img_show.setImageBitmap(bitmap);
+        }
+    }
+
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_start:
-                videoView.start();
-                break;
-            case R.id.btn_pause:
-                videoView.pause();
-                break;
-            case R.id.btn_stop:
-                videoView.stopPlayback();
-                break;
-        }
+        //
+        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(it,Activity.DEFAULT_KEYS_DIALER);
+
     }
 }
